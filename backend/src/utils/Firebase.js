e40821e80acdac05bbcc70bdc1d56be88386b4ad
denemesafter <denemesafter@gmail.com>
2024-06-05 02:15:15 +0300

    change how to init firebase credentials

--- backend/src/utils/Firebase.js
+++ backend/src/utils/Firebase.js
@@ -1,21 +1,28 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = {
-  type: 'service_account',
-  universe_domain: "googleapis.com",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,    
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+
+
+const getServiceAccountCredential = () => {
+  const serviceAccount = {
+    type: 'service_account',
+    universe_domain: "googleapis.com",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,    
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  };
+  return serviceAccount
+}
+
+const serviceAccountCredential =  getServiceAccountCredential()
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccountCredential)
 });
 
 
--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,20 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-console.log("process.env.VUE_APP_FIREBASE_API_KEY",process.env.VUE_APP_FIREBASE_API_KEY)
-console.log("VUE_APP_FIREBASE_PROJECT_ID",process.env.VUE_APP_FIREBASE_PROJECT_ID)
-console.log("VUE_APP_API_TOKEN",process.env.VUE_APP_API_TOKEN)
-console.log("API_TOKEN",process.env.API_TOKEN)
 
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
-};
+const getFirebaseConfig = () => {
+  console.log("process.env.VUE_APP_FIREBASE_API_KEY",process.env.VUE_APP_FIREBASE_API_KEY)
+  console.log("VUE_APP_FIREBASE_PROJECT_ID",process.env.VUE_APP_FIREBASE_PROJECT_ID)
+  console.log("VUE_APP_API_TOKEN",process.env.VUE_APP_API_TOKEN)
+  console.log("API_TOKEN",process.env.API_TOKEN)
+  
+  const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+  };
+  return firebaseConfig
+}
+
+const firebaseConfig = getFirebaseConfig()
 initializeApp(firebaseConfig);
 
 const auth = getAuth();
