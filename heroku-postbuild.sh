7606fa31d03c347656a294cab5e69ccbc86f486b
denemesafter <denemesafter@gmail.com>
2024-06-05 01:24:17 +0300

    some trials

--- heroku-postbuild.sh
+++ heroku-postbuild.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -e
+
+echo "Building frontend..."
 cd frontend
 yarn install
 yarn run build
 cd ..
+
+echo "Frontend build complete. Checking files..."
+ls frontend
+ls -l frontend/dist
+
+echo "Building backend..."
 cd backend
 yarn install
